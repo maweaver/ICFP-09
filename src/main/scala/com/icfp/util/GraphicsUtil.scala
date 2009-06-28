7e251c2781fbdc0c4433da5080756abdb75e37b5
Matt Weaver <matt@innerweaver.com>
2009-06-27 21:48:44 -0400

    Y-Coordinate flipped back

--- src/main/scala/com/icfp/util/GraphicsUtil.scala
+++ src/main/scala/com/icfp/util/GraphicsUtil.scala
@@ -7,7 +7,7 @@ object GraphicsUtil {
   def globalToLocal(imageSize: Dimension, maxRadius: Double, x: Double, y: Double): (Double, Double) = {
     val minDim = Math.min(imageSize.getWidth, imageSize.getHeight)
     (imageSize.getWidth / 2.0d + ((x / maxRadius) * minDim) / 2.0d,
-     imageSize.getHeight / 2.0d + ((y / maxRadius) * minDim) / 2.0d)
+     imageSize.getHeight / 2.0d - ((y / maxRadius) * minDim) / 2.0d)
   }
   
   def drawRadius(g: Graphics, imageSize: Dimension, maxRadius: Double, radius: Double, color: Color, fill: Boolean) {
@@ -16,8 +16,8 @@ object GraphicsUtil {
     
     val percentOut = radius / maxRadius
 
-    val ul = globalToLocal(imageSize, maxRadius, -radius, -radius)
-    val lr = globalToLocal(imageSize, maxRadius, radius, radius)
+    val ul = globalToLocal(imageSize, maxRadius, -radius, radius)
+    val lr = globalToLocal(imageSize, maxRadius, radius, -radius)
     
     if(fill) {
       g.fillOval(ul._1.toInt, ul._2.toInt, (lr._1 - ul._1).toInt, (lr._2 - ul._2).toInt)
